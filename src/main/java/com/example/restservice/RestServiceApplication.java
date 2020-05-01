1c7e36e9202b3f1931f971f6a04639df7cafc43a
leesk8012 <leesk8012@gmail.com>
2020-05-01 15:12:55 +0900

    Update configuration for standalone tomcat

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,12 +2,21 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// ADDED
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class RestServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+    // ADDED
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestServiceApplication.class);
     }
 
 }
